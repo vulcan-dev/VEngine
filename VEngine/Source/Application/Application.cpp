8d89c07cbfaab8365f8f88c78b5b6978493146b9
vulcan-dev <vitexuhd@gmail.com>
2021-04-12 17:29:37 +0100

    Commented some stuff

--- VEngine/Source/Application/Application.cpp
+++ VEngine/Source/Application/Application.cpp
@@ -1,12 +1,16 @@
 #include "Application.h"
 
 void Application::Initialize() {
+	// Initialize the window
 	this->_window.Initialize();
+
+	// Added this for testing but I'll keep it for now
 	if (this->_window.getWindow() == nullptr) {
-		VEngine::Utilities::Log._logger->critical("_window.getWindow() is nullptr");
+		VEngine::Utilities::Log._logger->critical("_window.getWindow() is nullptr. Closing application");
 		glfwTerminate();
 	}
 
+	// Set the current context to this window
 	glfwMakeContextCurrent(this->_window.getWindow());
 
 	VEngine::Utilities::Log._logger->info("Application initialized");
@@ -17,13 +21,13 @@ void Application::Update() {
 	while (this->_window.isRunning()) {
 		this->Render();
 
-		glfwPollEvents();
+		glfwPollEvents(); // glfw handles the events so we need to call this function every update. if we press a key, close the window, etc.. this handles it
 	}
 }
 
 void Application::Render() {
-	glClearColor(1.f, 0.f, 0.f, 1.f);
+	glClearColor(.2f, .2f, .2f, 1.f); // set the background colour of the window
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	glfwSwapBuffers(this->_window.getWindow());
+	glfwSwapBuffers(this->_window.getWindow()); // swaps the buffers (as u can see)
 }
