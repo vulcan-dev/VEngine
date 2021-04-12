5ba847840f825ac5e09745ab88b38b8666bb88e6
vulcan-dev <vitexuhd@gmail.com>
2021-04-12 17:20:59 +0100

    Added Render function. Window now has the OpenGL context and can render

--- VEngine/Source/Application/Application.cpp
+++ VEngine/Source/Application/Application.cpp
@@ -2,6 +2,12 @@
 
 void Application::Initialize() {
 	this->_window.Initialize();
+	if (this->_window.getWindow() == nullptr) {
+		VEngine::Utilities::Log._logger->critical("_window.getWindow() is nullptr");
+		glfwTerminate();
+	}
+
+	glfwMakeContextCurrent(this->_window.getWindow());
 
 	VEngine::Utilities::Log._logger->info("Application initialized");
 }
@@ -9,6 +15,15 @@ void Application::Initialize() {
 void Application::Update() {
 	VEngine::Utilities::Log._logger->info("Application updating");
 	while (this->_window.isRunning()) {
+		this->Render();
+
 		glfwPollEvents();
 	}
+}
+
+void Application::Render() {
+	glClearColor(1.f, 0.f, 0.f, 1.f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glfwSwapBuffers(this->_window.getWindow());
 }
--- VEngine/Source/Application/Application.h
+++ VEngine/Source/Application/Application.h
@@ -6,6 +6,7 @@ class Application {
 public:
 	void Initialize();
 	void Update();
+	void Render();
 
 private:
 	static constexpr uint32_t windowWidth = 1280;
